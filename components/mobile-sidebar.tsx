2ea208541103e10bcbefbb0cbac4d670102c8e68
Seronic <shubhadeepmandal11@gmail.com>
2023-09-01 12:00:48 +0530

    changed landing navbar

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -4,7 +4,6 @@ import { Button } from "@/components/ui/button";
 import { Menu } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Sidebar from "@/components/sidebar";
-import { useEffect, useState } from "react";
 
 interface MobileSidebarProps {
     apiLimitCount: number;
@@ -15,22 +14,14 @@ const MobileSidebar = ({
     apiLimitCount = 0,
     isPro = false,
 }:MobileSidebarProps) => {
-
-    const [isMounted, setIsMounted] = useState(false);
-
-    useEffect(() => {
-        setIsMounted(true);
-    }, []);
-
-    if (!isMounted) return null;
     
   return (
     <Sheet>
-        <SheetTrigger>
         <Button variant="ghost" size="icon" className="md:hidden" asChild>
+        <SheetTrigger>
             <Menu/>
-        </Button>
         </SheetTrigger>
+        </Button>
         <SheetContent side="left" className="p-0">
             <Sidebar isPro={isPro} apiLimitCount={apiLimitCount} />
         </SheetContent>
